197f4a2e162c81b8f02ee6d798f72019186dd515
Nicolas Renaud <n.renaud@tudelft.nl>
2018-08-22 16:11:13 +0200

    big fix

--- pssmgen/pssm.py
+++ pssmgen/pssm.py
@@ -1,16 +1,17 @@
+import os
 from pdb2sql.pdb2sqlcore import pdb2sql
-from .map_pssm2pdb import write_mapped_pssm_pdb
-from Bio.Blast.Applications import NcbipsiblastCommandline
 import shutil
+from Bio.Blast.Applications import NcbipsiblastCommandline
+from .map_pssm2pdb import write_mapped_pssm_pdb
 
-class PSSM(object):
 
-    def __init__(self,pdb,fasta,pssm):
+class PSSMdecoy(object):
 
-        self.caseID = os.path.splitext(os.path.basename(pdb))[0]
-        self.pdb = pdb2sql(pdb)
-        self.fasta = fasta
-        self.pssm = pssm
+    def __init__(self,caseID,pdbdir='pdb'):
+
+        self.caseID = caseID
+        self.pdbdir = pdbdir
+        self.pdbs = os.listdir(os.path.join(self.caseID,self.pdbdir))
 
         self.One2ThreeDict = {
         'A' : 'ALA', 'R' : 'ARG', 'N' : 'ASN', 'D' : 'ASP', 'C' : 'CYS', 'E' : 'GLU', 'Q' : 'GLN',
@@ -19,15 +20,21 @@ class PSSM(object):
         'B' : 'ASX', 'U' : 'SEC', 'Z' : 'GLX'
         }
 
-        self.Three2OneDict = {v: k for k, v in resmap.items()}
+        self.Three2OneDict = {v: k for k, v in self.One2ThreeDict.items()}
 
 
-    def get_fasta(self,chain=['A','B']):
+    def get_fasta(self,chain=['A','B'],outdir='./fasta/'):
 
+        outdir = os.path.join(self.caseID,outdir)
+        if not os.path.isfile(outdir):
+            os.mkdir(outdir)
+
+        pdb = os.path.join(os.path.join(self.caseID,self.pdbdir),self.pdbs[0])
+        sqldb = pdb2sql(pdb)
         for c in chain:
 
             # get the unique residues
-            res = pdb.get_residues(chainID=c)
+            res = sqldb.get_residues(chainID=c)
 
             # get the one letter resiude
             seq = ''
@@ -35,33 +42,65 @@ class PSSM(object):
                 seq += self.Three2OneDict[r[1]]
 
             # write the file
-            fname = self.caseID + '_%s' %c + '.fasta'
-            f = open('fname','w')
+            fname = os.path.join(outdir,self.caseID + '_%s' %c + '.fasta')
+            f = open(fname,'w')
             f.write('>%s' %self.caseID + '_%s' %c)
             f.write(seq)
             f.close()
+        sqldb.close()
 
-    def get_pssm(self,blast,db,query,out):
 
-        name = os.path.splitext(os.path.basename(query))[0]
+    def get_pssm(self,fasta_dir='./fasta/',
+                 blast = '/home/clgeng/software/blast/bin/psiblast',
+                 db = '/data/lixue/DBs/blast_dbs/nr_v20180204/nr',
+                 outdir='./pssm_raw/'):
 
-        psi_cline = NcbipsiblastCommandline(
-                           blast,
-                           db = db,
-                           query = query,
-                           evalue = 0.0001,
-                           matrix='BLOSUM62',
-                           outfmt = 7,
-                           save_each_pssm=True,
-                           num_iterations=3,
-                           save_pssm_after_last_round=True,
-                           out_ascii_pssm=name + '.pssm',
-                           out_pssm = name + '.cptpssm',
-                           out = name + '.xml',
-                           )
+        fasta_dir = os.path.join(self.caseID,fasta_dir)
+        outdir = os.path.join(self.caseID,outdir)
+        if not os.path.isfile(outdir):
+            os.mkdir(outdir)
 
-        shutil.copy2(name+ '.pssm.3', out)
+        for q in os.listdir(fasta_dir):
 
-    def map_pssm(self,fpssm,fpdb,chainID,outdir):
-        write_mapped_pssm_pdb(fpssm, fpdb, chainID, outdir)
+            query = os.path.join(fasta_dir,q)
+            name = os.path.splitext(os.path.basename(query))[0]
+
+            psi_cline = NcbipsiblastCommandline(
+                               blast,
+                               db = db,
+                               query = query,
+                               evalue = 0.0001,
+                               matrix='BLOSUM62',
+                               outfmt = 7,
+                               save_each_pssm=True,
+                               num_iterations=3,
+                               save_pssm_after_last_round=True,
+                               out_ascii_pssm=name + '.pssm',
+                               out_pssm = name + '.cptpssm',
+                               out = name + '.xml',
+                               )
+
+            name = os.path.split(os.path.basename(query))[0] = '.pssm'
+            out = os.path.join(outdir,name)
+            shutil.copy2(name+ '.pssm.3', out)
+
+
+    def map_pssm(self,pssm_dir='pssm_raw',outdir='pssm',chain=['A','B']):
+
+        pssm_dir = os.path.join(self.caseID,pssm_dir)
+        outdir = os.path.join(self.caseID,outdir)
+        if not os.path.isfile(outdir):
+            os.mkdir(outdir)
+
+        pf = os.listdir(pssm_dir)
+        pssm_files = {}
+        for c in chain:
+            pssm_files[c] = list(filter(lambda x: x.endswith(c+'.pssm'),pf))[0]
+
+        for p in self.pdbs:
+            pdb = os.path.join(os.path.join(self.caseID,self.pdbdir),p)
+            for c in chain:
+                pssm = os.path.join(pssm_dir,pssm_files[c])
+                print(pssm)
+                write_mapped_pssm_pdb(pssm, pdb, c, outdir)
 

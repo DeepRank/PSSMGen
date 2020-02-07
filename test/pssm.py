7b46f295708afa33ff6a24498a79b3ef8e2536e7
Cunliang Geng <c.geng@esciencecenter.nl>
2020-02-07 09:29:41 +0100

    update test code

--- test/pssm.py
+++ test/pssm.py
@@ -1,16 +1,23 @@
 from pssmgen.pssm import PSSM
 
-gen = PSSM('1AK4')
+# initiate the PSSM object
+gen = PSSM(work_dir='7CEI')
 
-# set blast excuete and database
+# set psiblast executable, database and other psiblast parameters (here shows the defaults)
 gen.configure(blast_exe='/home/software/blast/bin/psiblast',
-            database='/data/DBs/blast_dbs/nr_v20180204/nr')
+            database='/data/DBs/blast_dbs/nr_v20180204/nr',
+            num_threads = 4, evalue=0.0001, comp_based_stats='T',
+            max_target_seqs=2000, num_iterations=3, outfmt=7,
+            save_each_pssm=True, save_pssm_after_last_round=True)
 
-# generates the FASTA query
- gen.get_fasta()
+# generates FASTA files
+gen.get_fasta(pdb_dir='pdb', chain=['A','B'], out_dir='fasta')
 
-# generates the PSSM
- gen.get_pssm(run=False)
+# generates PSSM
+gen.get_pssm(fasta_dir='fasta', out_dir='pssm_raw', run=True)
 
-# map the pssm to the pdb
-gen.map_pssm()
+# map PSSM and PDB to get consisitent files
+gen.map_pssm(pssm_dir='pssm_raw', pdb_dir='pdb', out_dir='pssm', chain=['A','B'])
+
+# write consistent files and move
+gen.get_mapped_pdb(pdbpssm_dir='pssm', pdb_dir='pdb', pdbnonmatch_dir='pdb_nonmatch')

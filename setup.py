f3f72e523c7960fd8f57950a1d7a606a1b61217d
Cunliang Geng <c.geng@esciencecenter.nl>
2020-02-07 13:17:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ version = {}
 with open(os.path.join(cwd, 'pssmgen', '__version__.py')) as f:
     exec(f.read(), version)
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='PSSMGen',
-    description='Generates consistent PSSM and/or PDB files for protein-protein complexes',
     version=version['__version__'],
+    description='Generates consistent PSSM and/or PDB files for protein-protein complexes',
+    long_description=readme + '\n\n',
+    long_description_content_type='text/markdown',
     author='Nicolas Renaud, Cunliang Geng',
     author_email='n.renaud@esciencecenter.nl, c.geng@esciencecenter.nl',
     url='https://github.com/DeepRank/PSSMGen',

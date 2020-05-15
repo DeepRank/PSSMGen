8e4af8f4d02a1647babf3e2a0ef997f20ecfeb4f
Cunliang Geng <c.geng@esciencecenter.nl>
2020-05-15 14:11:00 +0200

    rename pssm to test_pssm

--- test/test_pssm.py
+++ test/test_pssm.py
@@ -11,13 +11,13 @@ gen.configure(blast_exe='/home/software/blast/bin/psiblast',
             save_each_pssm=True, save_pssm_after_last_round=True)
 
 # generates FASTA files
-gen.get_fasta(pdb_dir='pdb', chain=['A','B'], out_dir='fasta')
+gen.get_fasta(pdb_dir='pdb', chain=('A','B'), out_dir='fasta')
 
 # generates PSSM
 gen.get_pssm(fasta_dir='fasta', out_dir='pssm_raw', run=True)
 
 # map PSSM and PDB to get consisitent files
-gen.map_pssm(pssm_dir='pssm_raw', pdb_dir='pdb', out_dir='pssm', chain=['A','B'])
+gen.map_pssm(pssm_dir='pssm_raw', pdb_dir='pdb', out_dir='pssm', chain=('A','B'))
 
 # write consistent files and move
 gen.get_mapped_pdb(pdbpssm_dir='pssm', pdb_dir='pdb', pdbnonmatch_dir='pdb_nonmatch')
